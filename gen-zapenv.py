98835ba83673dd4bad608f1f58006296e9620242
Vital A Pi <code@vp.ag>
2021-11-19 10:29:31 +0300

    zap cfg gen with templates

--- gen-zapenv.py
+++ gen-zapenv.py
@@ -1,15 +1,17 @@
 import argparse
+import jinja2
 import json
 import jsonschema
-import pathlib
 import sys
+from pathlib import Path
 
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-o', '--out-dir', default='.')
+parser.add_argument('-t', '--templates-dir', default='./templates-zap')
 args = parser.parse_args()
 
-jsch = {"type": "object", "required": ["endpoints"], "additionalProperties": False, "properties": {
+cfg_sch = {"type": "object", "required": ["endpoints"], "additionalProperties": False, "properties": {
     "endpoints": {"type": "array",
         "items": {"type": "string"}},
     # "header": {"type": "object", "properties": {
@@ -18,19 +20,44 @@ jsch = {"type": "object", "required": ["endpoints"], "additionalProperties": Fal
     "headers": {"type": "array",
         "items": {"type": "array", "minItems": 2, "maxItems": 2,
             "prefixItems": [{"type": "string"}, {"type": "string"}]}},
-    "oas": {"oneOf": [
-        {"type": "object", "properties":
-            {"file": {"type": "string"}}},
-        {"type": "object", "properties":
-            {"url": {"type": "string"}}}
-    ]}
+    # "oas": {"oneOf": [
+    #     {"type": "object", "properties":
+    #         {"file": {"type": "string"}}},
+    #     {"type": "object", "properties":
+    #         {"url": {"type": "string"}}}
+    # ]}
+    "oas": {"type": "object",
+        "properties": {
+            "file": {"type": "string"},
+            "url": {"type": "string"}},
+        "oneOf": [
+            {"required": ["file"]},
+            {"required": ["url"]}]}
 }}
 
-print(args.out_dir)
 
 try:
-    # data = json.load(sys.stdin)
-    jsonschema.validate(json.load(sys.stdin), schema=jsch)
+    cfg = json.load(sys.stdin)
+    jsonschema.validate(cfg, schema=cfg_sch)
 except Exception as e:
     print(type(e).__name__, str(e))
     sys.exit(1)
+
+jenv = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(args.templates_dir),
+    trim_blocks=True, lstrip_blocks=True, keep_trailing_newline=True)
+
+jenv.globals = cfg
+
+for template_file in [
+        'zap-af.yaml.j2',      # urls
+        'zap-options.cfg.j2',  # (Not parametrized yet)
+        'hsendr.py.j2',        # headers
+        ]:
+    try:
+        if template_rendered := jenv.get_template(template_file).render():
+            with open(Path(args.out_dir, Path(template_file).stem), 'w') as fo:
+                fo.write(template_rendered)
+    except Exception as e:
+        print(type(e).__name__, str(e))
+        sys.exit(1)
