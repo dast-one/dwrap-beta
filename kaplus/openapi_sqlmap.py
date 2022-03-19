7a75cab287e24cf563fa335867b1171a4b30de42
Vital A Pi <code@vp.ag>
2022-03-19 17:25:15 +0300

    sqlmap-jockey upd

--- kaplus/openapi_sqlmap.py
+++ kaplus/openapi_sqlmap.py
@@ -12,12 +12,19 @@ path_param = re.compile(r'\{([^}]+?)\}')
 
 def _sample_for_schema(sd):
     """Returns a sample for given schema dict."""
+    if sd is None:
+        return
+
     if sd['type'] == 'object':
-        return dict((p, _sample_for_schema(s)) for (p, s) in sd['properties'].items())
+        return dict((p, _sample_for_schema(s)) for (p, s) in sd.get('properties', dict()).items())
+    elif sd['type'] == 'array':
+        return list(_sample_for_schema(sd['items']) for _ in range(3))
     else:
         return {
             'string': 'qwe',
             'integer': 123,
+            'number': 123,
+            'boolean': True,
         }.get(sd['type'], sd['type'] + '(WTF?!)')
     # TODO: Support other types or use full-featured generator.
 
@@ -28,21 +35,32 @@ def sqlmap_tasks(oas):
         smt = dict()
         smt['url'] = path_param.sub(r'\1*', u)
         smt['headers'] = list()
+        #_HACK_2h18.1_# smt['url'] += '?apikey=...'
         for m in oas['paths'][u]:
             smt['method'] = m.upper()
 
             if smt['method'] == 'GET':
-                if get_params := '&'.join('{}={}'.format(
+                if qry_params := '&'.join('{}={}'.format(
                     p['name'], _sample_for_schema(p['schema']))
                         for p in oas['paths'][u][m].get('parameters', dict())
                         if p.get('in') == 'query'
                 ):
-                    smt['url'] += f'&{get_params}'
+                    smt['url'] += f'?{qry_params}'
+                    #_HACK_2h18.1_# smt['url'] += f'&{qry_params}'
                 # TODO: Support in-header params.
 
             elif smt['method'] == 'POST':
+                if qry_params := '&'.join('{}={}'.format(
+                    p['name'], _sample_for_schema(p['schema']))
+                        for p in oas['paths'][u][m].get('parameters', dict())
+                        if p.get('in') == 'query'
+                ):
+                    smt['url'] += f'?{qry_params}'
+                    #_HACK_2h18.1_# smt['url'] += f'&{qry_params}'
+                # TODO: Support in-header params.
+
                 if post_data := _sample_for_schema(
-                    oas['paths'][u][m]['requestBody']['content']['application/json']['schema']
+                        oas['paths'][u][m].get('requestBody', {'content': {'application/json': {'schema': None}}})['content']['application/json']['schema']
                 ):
                     smt['headers'].append('content-type:application/json')
                     smt['data'] = json.dumps(post_data)
--- kaplus/sqlmap-jockey.py
+++ kaplus/sqlmap-jockey.py
@@ -4,7 +4,7 @@ import pathlib
 import sys
 import threading
 
-sys.path.insert(1, pathlib.Path('~/src/sqlmap').expanduser().as_posix())
+sys.path.insert(1, pathlib.Path('/usr/share/sqlmap').expanduser().as_posix())
 from lib.utils.api import *
 from lib.utils.api import _client
 from thirdparty.six.moves import http_client as _http_client
@@ -16,116 +16,126 @@ from thirdparty.six.moves import urllib as _urllib
 from openapi_sqlmap import oas_load, sqlmap_tasks
 
 
+class SqlmapApiClient:
+    """Based on sqlmapapi client.
+
+    [lib/utils/api.py](https://github.com/sqlmapproject/sqlmap/blob/master/lib/utils/api.py)
+    """
+
+    def __init__(
+        self,
+        host=RESTAPI_DEFAULT_ADDRESS, port=RESTAPI_DEFAULT_PORT,
+        username=None, password=None
+    ):
+        DataStore.username = username
+        DataStore.password = password
+
+        self.addr = "http://%s:%d" % (host, port)
+        logger.info("Starting REST-JSON API client to '%s'..." % self.addr)
+
+        try:
+            _client(self.addr)
+        except Exception as ex:
+            if not isinstance(ex, _urllib.error.HTTPError) or ex.code == _http_client.UNAUTHORIZED:
+                errMsg = "There has been a problem while connecting to the "
+                errMsg += "REST-JSON API server at '%s' " % self.addr
+                errMsg += "(%s)" % getSafeExString(ex)
+                logger.critical(errMsg)
+                return
+
+    def new_scan_w(self, command):
+        """Create and start new scan, waiting for finish.
+        """
+        if isinstance(command, str):
+            try:
+                argv = ["sqlmap.py"] + shlex.split(command)[1:]
+            except Exception as ex:
+                logger.error("Error occurred while parsing arguments ('%s')" % getSafeExString(ex))
+                taskid = None
+                raise
+        elif all(isinstance(x, str) for x in command):
+            argv = list(command)
+        else:
+            logger.error("Cannot decide WTF is ('%s')" % getSafeExString(ex))
+
+        logger.info(argv)
+
+        try:
+            cmdLineOptions = cmdLineParser(argv).__dict__
+        except:
+            taskid = None
+            raise
+
+        for key in list(cmdLineOptions):
+            if cmdLineOptions[key] is None:
+                del cmdLineOptions[key]
+
+        raw = _client("%s/task/new" % self.addr)
+        res = dejsonize(raw)
+        if not res["success"]:
+            logger.error("Failed to create new task ('%s')" % res.get("message", ""))
+            raise
+        taskid = res["taskid"]
+        logger.info("New task ID is '%s'" % taskid)
+
+        raw = _client("%s/scan/%s/start" % (self.addr, taskid), cmdLineOptions)
+        res = dejsonize(raw)
+        if not res["success"]:
+            logger.error("Failed to start scan ('%s')" % res.get("message", ""))
+            raise
+        logger.info(f"[{taskid}] started")
+
+        # if command in ("data", "log", "status", "stop", "kill"):
+        #     if not taskid:
+        #         logger.error("No task ID in use")
+        #         continue
+        #     raw = _client("%s/scan/%s/%s" % (addr, taskid, command))
+        #     res = dejsonize(raw)
+        #     if not res["success"]:
+        #         logger.error("Failed to execute command %s" % command)
+        #     dataToStdout("%s\n" % raw)
+
+        
+        while dejsonize(_client("%s/scan/%s/%s" % (self.addr, taskid, 'status'))).get('status') == 'running':
+            time.sleep(2)
+        logger.info(f"[{taskid}] finished")
+
+    def list_tasks(self):
+        command = 'list'
+        raw = _client("%s/admin/%s" % (self.addr, command))
+        res = dejsonize(raw)
+        if not res["success"]:
+            logger.error("Failed to execute command %s" % command)
+        elif command == "flush":
+            taskid = None
+        dataToStdout("%s\n" % raw)
 
 
-
-def sqlmapapi_client(host=RESTAPI_DEFAULT_ADDRESS, port=RESTAPI_DEFAULT_PORT, username=None, password=None):
-    DataStore.username = username
-    DataStore.password = password
-
-    dbgMsg = "Example client access from command line:"
-    dbgMsg += "\n\t$ taskid=$(curl http://%s:%d/task/new 2>1 | grep -o -I '[a-f0-9]\\{16\\}') && echo $taskid" % (host, port)
-    dbgMsg += "\n\t$ curl -H \"Content-Type: application/json\" -X POST -d '{\"url\": \"http://testphp.vulnweb.com/artists.php?artist=1\"}' http://%s:%d/scan/$taskid/start" % (host, port)
-    dbgMsg += "\n\t$ curl http://%s:%d/scan/$taskid/data" % (host, port)
-    dbgMsg += "\n\t$ curl http://%s:%d/scan/$taskid/log" % (host, port)
-    logger.debug(dbgMsg)
-
-    addr = "http://%s:%d" % (host, port)
-    logger.info("Starting REST-JSON API client to '%s'..." % addr)
-
-    try:
-        _client(addr)
-    except Exception as ex:
-        if not isinstance(ex, _urllib.error.HTTPError) or ex.code == _http_client.UNAUTHORIZED:
-            errMsg = "There has been a problem while connecting to the "
-            errMsg += "REST-JSON API server at '%s' " % addr
-            errMsg += "(%s)" % getSafeExString(ex)
-            logger.critical(errMsg)
-            return
+def thread_worker(sqlmap_task):
+    with pool_sema:
+        # logger.debug(sqlmap_task)
+        sm.new_scan_w(sqlmap_task)
 
 
-
-    ##################################
-    # elif command in ("list", "flush"):
-    command = 'new -u 172.21.0.11:10013/tokens -X POST --data \'{"username":"U","password":"P"}\' --ignore-code=\'*\''
-    try:
-        argv = ["sqlmap.py"] + shlex.split(command)[1:]
-    except Exception as ex:
-        logger.error("Error occurred while parsing arguments ('%s')" % getSafeExString(ex))
-        taskid = None
-        raise
-
-    try:
-        cmdLineOptions = cmdLineParser(argv).__dict__
-    except:
-        taskid = None
-        raise
-
-    for key in list(cmdLineOptions):
-        if cmdLineOptions[key] is None:
-            del cmdLineOptions[key]
-
-    raw = _client("%s/task/new" % addr)
-    res = dejsonize(raw)
-    if not res["success"]:
-        logger.error("Failed to create new task ('%s')" % res.get("message", ""))
-        raise
-    taskid = res["taskid"]
-    logger.info("New task ID is '%s'" % taskid)
-
-    raw = _client("%s/scan/%s/start" % (addr, taskid), cmdLineOptions)
-    res = dejsonize(raw)
-    if not res["success"]:
-        logger.error("Failed to start scan ('%s')" % res.get("message", ""))
-        raise
-    logger.info("Scanning started")
-
-    ##################################
-    # elif command in ("list", "flush"):
-    command = 'list'
-    raw = _client("%s/admin/%s" % (addr, command))
-    res = dejsonize(raw)
-    if not res["success"]:
-        logger.error("Failed to execute command %s" % command)
-    elif command == "flush":
-        taskid = None
-    dataToStdout("%s\n" % raw)
-
-
-
-
-
+logger.setLevel(logging.DEBUG)
 
 max_threads = 3
-
 pool_sema = threading.BoundedSemaphore(max_threads)
 
+sm = SqlmapApiClient()
+sm.list_tasks()
 
-def thread_task(sqlmap_task):
-    with pool_sema:
-        delta_t = random.randint(3, 10)
-        time.sleep(random.random())
-        print(f'task {task_id} started with delta {delta_t}', args)
-        time.sleep(delta_t)
-        print(f'task {task_id} finished')
+base_url = 'http://localhost:10013'
 
+for smt in sqlmap_tasks(oas_load('/hka/oas.yaml')):
+    u = '/'.join((base_url.rstrip('/'), smt['url'].lstrip('/')))
+    t = threading.Thread(
+        target=thread_worker,
+        args=(
+            f"new -u {u} -X {smt['method']} -H 'content-type:application/json'"
+            + (f" --data '{smt['data']}'" if smt.get('data') else '')
+            + ' --random-agent --level=5 --risk=3 --skip="Host,Referer,User-Agent" --ignore-code=*',
+        )
+    )
+    t.start()
 
-
-for smt in sqlmap_tasks(oas_load('~/tmp/masc/oapi-specs/vAPI.yaml')):
-    print(smt)
-    # t = threading.Thread(target=thread_task, args=smt)
-    # t.start()
-
-sqlmapapi_client(host='172.21.0.11')
-
-# def task_new():
-#     """
-#     Create a new task
-#     """
-#     taskid = encodeHex(os.urandom(8), binary=False)
-#     remote_addr = request.remote_addr
-#
-#     DataStore.tasks[taskid] = Task(taskid, remote_addr)
-#
-#     logger.debug("Created new task: '%s'" % taskid)
-#     return jsonize({"success": True, "taskid": taskid})
