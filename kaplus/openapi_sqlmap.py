9f3ef84f9d7ed161d182d41e494fcbe74fdd21a0
Vital A Pi <code@vp.ag>
2022-04-17 19:31:37 +0300

    sqlmap task generator: * parametrized with additional query params * dereferencing enabled

--- kaplus/openapi_sqlmap.py
+++ kaplus/openapi_sqlmap.py
@@ -1,5 +1,4 @@
-"""Supplementary module: openapi spec -> sqlmap tasks.
-"""
+"""Supplementary module: openapi spec -> sqlmap tasks."""
 
 import json
 import os
@@ -10,15 +9,21 @@ import yaml
 
 path_param = re.compile(r'\{([^}]+?)\}')
 
-def _sample_for_schema(sd):
-    """Returns a sample for given schema dict."""
+def _sample_for_schema(sd, oas):
+    """Returns a sample for given schema dict, dereferencing with given oapispec."""
     if sd is None:
         return
 
+    if ref := sd.get('$ref'):
+        d = oas
+        for k in ref.split('/')[1:]:
+            d = d[k]
+        return _sample_for_schema(d, oas)
+
     if sd['type'] == 'object':
-        return dict((p, _sample_for_schema(s)) for (p, s) in sd.get('properties', dict()).items())
+        return dict((p, _sample_for_schema(s, oas)) for (p, s) in sd.get('properties', dict()).items())
     elif sd['type'] == 'array':
-        return list(_sample_for_schema(sd['items']) for _ in range(3))
+        return list(_sample_for_schema(sd['items'], oas) for _ in range(3))
     else:
         return {
             'string': 'qwe',
@@ -28,42 +33,47 @@ def _sample_for_schema(sd):
         }.get(sd['type'], sd['type'] + '(WTF?!)')
     # TODO: Support other types or use full-featured generator.
 
-def sqlmap_tasks(oas):
-    """Create sqlmap tasks from openapi spec."""
+def sqlmap_tasks(oas, additional_qryparams=[]):
+    """Create sqlmap tasks from openapi spec.
+
+    Additional query params - a list of strings,
+    like ['apikey=<token>', 'smthelse=...']
+    """
     tasks = list()
     for u in oas['paths']:
         smt = dict()
         smt['url'] = path_param.sub(r'\1*', u)
         smt['headers'] = list()
-        #_HACK_2h18.1_# smt['url'] += '?apikey=...'
         for m in oas['paths'][u]:
             smt['method'] = m.upper()
 
             if smt['method'] == 'GET':
-                if qry_params := '&'.join('{}={}'.format(
-                    p['name'], _sample_for_schema(p['schema']))
+                if qry_params := '&'.join(
+                    additional_qryparams
+                    + ['{}={}'.format(p['name'], _sample_for_schema(p['schema'], oas))
                         for p in oas['paths'][u][m].get('parameters', dict())
-                        if p.get('in') == 'query'
+                        if p.get('in') == 'query']
                 ):
                     smt['url'] += f'?{qry_params}'
-                    #_HACK_2h18.1_# smt['url'] += f'&{qry_params}'
                 # TODO: Support in-header params.
 
             elif smt['method'] == 'POST':
-                if qry_params := '&'.join('{}={}'.format(
-                    p['name'], _sample_for_schema(p['schema']))
+                if qry_params := '&'.join(
+                    additional_qryparams
+                    + ['{}={}'.format(p['name'], _sample_for_schema(p['schema'], oas))
                         for p in oas['paths'][u][m].get('parameters', dict())
-                        if p.get('in') == 'query'
+                        if p.get('in') == 'query']
                 ):
                     smt['url'] += f'?{qry_params}'
-                    #_HACK_2h18.1_# smt['url'] += f'&{qry_params}'
                 # TODO: Support in-header params.
 
                 if post_data := _sample_for_schema(
-                        oas['paths'][u][m].get('requestBody', {'content': {'application/json': {'schema': None}}})['content']['application/json']['schema']
+                        oas['paths'][u][m].get('requestBody', {'content': {'application/json': {'schema': None}}})['content']['application/json']['schema'],
+                        oas
                 ):
                     smt['headers'].append('content-type:application/json')
                     smt['data'] = json.dumps(post_data)
+                # TODO: HAX hardcoded app/js contntype
                 # TODO: Support in-header params.
 
             # TODO: Support other methods.
