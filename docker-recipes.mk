3eb3c75acce4cac8dd561492e99a26f5ab80eb23
Vital A Pi <code@vp.ag>
2022-05-27 16:19:04 +0300

    make process simplified
    * always tagging as latest, and with git-commit-id if possible
    * source name is not required in makefiles, since using `docker build --pull`

--- docker-recipes.mk
+++ docker-recipes.mk
@@ -1,41 +1,29 @@
-TAG := $(shell git rev-parse --short HEAD)
-ifndef TAG
-$(error TAG is not set; _x3_ what's being built.)
-endif
+TAG := git-$(shell git rev-parse --short HEAD)
 
-GIT_BRANCH := $(shell git rev-parse --abbrev-ref HEAD)
-DOCKER_IMAGE := ${TARGET_NAME}:${TAG}
-D_IMG_LATEST := ${TARGET_NAME}:latest
+DOCKER_IMAGE := ${TARGET_NAME}:latest
+ifndef DOCKER_IMAGE
+$(error DOCKER_IMAGE is not set; _x3_ what's being built.)
+endif
 
 .DEFAULT_GOAL := express-build
 
 
-.PHONY: full
-full: | pull build
-
-.PHONY: pull
-pull:
-	@echo "[make]  ---- pull ----"
-	docker pull ${SOURCE_NAME}
-
 .PHONY: express-build
 express-build:
-	@echo "[make]  ---- build ----"
+	@echo "[make]  ---- express-build ----"
 	docker build -t ${DOCKER_IMAGE} .
 
-.PHONY: build
-build:
-	@echo "[make]  ---- build ----"
+.PHONY: full
+full:
+	@echo "[make]  ---- full-build ----"
 	docker build --pull --no-cache -t ${DOCKER_IMAGE} .
-ifeq ($(GIT_BRANCH), main)
-	docker tag ${DOCKER_IMAGE} ${D_IMG_LATEST}
-else
-	@echo "[make] Current branch is ${GIT_BRANCH}. Skipping TAGging as latest."
+ifdef TAG
+	docker tag ${DOCKER_IMAGE} ${TARGET_NAME}:${TAG}
 endif
 
 .PHONY: dump
 dump:
-	docker save ${D_IMG_LATEST} | xz -eT0 > "${TARGET_NAME}.txz"
+	docker save ${DOCKER_IMAGE} | xz -eT0 > "${TARGET_NAME}.txz"
 	openssl enc -aes-256-cbc -pbkdf2 -nosalt -pass 'pass:typoscramble'  \
 	  -in "${TARGET_NAME}.txz" -out "${TARGET_NAME}.txz.ebin"
 	sha256sum "${TARGET_NAME}.txz" "${TARGET_NAME}.txz.ebin" > "${TARGET_NAME}.sha256"
