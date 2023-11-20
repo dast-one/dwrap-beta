e0b33ccd8d331c19cef5d5e2958ea0dc4fe41d03
Vital A Pi <code@vp.ag>
2023-11-20 18:23:25 +0300

    * build-context to be passed to builds * make-dump* facility GOINGHAX

--- docker-recipes.mk
+++ docker-recipes.mk
@@ -13,31 +13,12 @@ DOCKER_IMAGE := ${TARGET_NAME}:latest
 .PHONY: express-build
 express-build:
 	@echo "[make]  ---- express-build ----"
-	docker build -t ${DOCKER_IMAGE} .
+	docker build --build-context d1kit=/opt/d1/src/dyna-misc/d1kit-pkg -t ${DOCKER_IMAGE} .
 
 .PHONY: full
 full:
 	@echo "[make]  ---- full-build ----"
-# 	docker build --pull --no-cache -t ${DOCKER_IMAGE} .
-	docker build --no-cache ${DKR_BLD_ADDITIONALS} -t ${DOCKER_IMAGE} .
+	docker build --build-context d1kit=/opt/d1/src/dyna-misc/d1kit-pkg --no-cache ${DKR_BLD_ADDITIONALS} -t ${DOCKER_IMAGE} .
 ifdef TAG
 	docker tag ${DOCKER_IMAGE} ${TARGET_NAME}:${TAG}
 endif
-
-.PHONY: dump
-dump:
-	docker save ${DOCKER_IMAGE} | xz -T0 > "${TARGET_NAME}.txz"
-	sha256sum "${TARGET_NAME}.txz" > "${TARGET_NAME}.sha256"
-ifdef SCP_TO
-	scp "${TARGET_NAME}.txz" "${TARGET_NAME}.sha256" "${SCP_TO}"
-	rm "${TARGET_NAME}.txz" "${TARGET_NAME}.sha256"
-endif
-
-.PHONY: scramble
-scramble:
-	openssl enc -aes-256-cbc -md sha1 -nosalt -pass 'pass:typoscramble'  \
-	  -in "${TARGET_NAME}.txz" -out "${TARGET_NAME}.txz.ebin"
-# 	openssl enc -aes-256-cbc -pbkdf2 -nosalt -pass 'pass:typoscramble'  \
-# 	  -in "${TARGET_NAME}.txz" -out "${TARGET_NAME}.txz.ebin"
-	sha256sum "${TARGET_NAME}.txz.ebin" >> "${TARGET_NAME}.sha256"
-	rm "${TARGET_NAME}.txz"
